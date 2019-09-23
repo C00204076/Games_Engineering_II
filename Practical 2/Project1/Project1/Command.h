6bdd9100e24518151979d67e1b73acad39ffd934
Brandon <brandonsd97@gmail.com>
2019-09-23 12:08:22 +0100

    Practial 2 Folder added

---
+++ Practical 2/Project1/Project1/Command.h
@@ -0,0 +1,7 @@
+//
+// C00204076
+// Brandon Seah-Dempsey
+// Started at 12:05 23 September 2019
+// Finished at
+// Time taken:
+// Known bugs:
---
+++ Practical 2/Project1/Project1/main.cpp
@@ -0,0 +1,14 @@
+//
+// C00204076
+// Brandon Seah-Dempsey 
+// Started at 11:57 23 September 2019
+// Finished at 
+// Time taken:
+// Known bugs:
+
+int main()
+{
+
+	return 0;
+	//return 1;
+}
