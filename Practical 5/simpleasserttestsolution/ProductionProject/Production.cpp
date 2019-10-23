fa7e73e34d5b4acb6a64515d59e68c2233221d63
C00204076 <C00204076@itcarlow.ie>
2019-10-23 15:57:44 +0100

    Assert methods added to Practical 5

--- Practical 5/simpleasserttestsolution/ProductionProject/Production.cpp
+++ Practical 5/simpleasserttestsolution/ProductionProject/Production.cpp
@@ -4,3 +4,33 @@ int Production::someMethod(int a)
 {
 	return a - a;
 }
+
+//
+int greaterThan(int a, int number)
+{
+	assert(a > number);
+
+	return a;
+}
+
+//
+int lessThan(int a, int number)
+{
+	assert(a < number);
+
+	return a;
+}
+
+//
+void maxNumbers(int list, int number)
+{
+	assert(list > number);
+}
+
+//
+int repeatedNumbers(int a, int number)
+{
+	assert(a == number);
+
+	return a;
+}
--- Practical 5/simpleasserttestsolution/ProductionProject/Production.h
+++ Practical 5/simpleasserttestsolution/ProductionProject/Production.h
@@ -1,4 +1,12 @@
-class Production{
+#include <cassert>
+
+class Production
+{
 public:
 	int someMethod(int a);
+
+	int greaterThan(int a, int number);
+	int lessThan(int a, int number);
+	void maxNumbers(int list, int number);
+	int repeatedNumbers(int a, int number);
 };
