8ec660b23e07d1c3d44f1d201d6ec2222e654527
C00204076 <C00204076@itcarlow.ie>
2020-01-05 20:19:39 +0000

    Unable to complete Erlang

--- Practical 6/ErlangIntergration/ErlangIntergration/ErlangIntergration.cpp
+++ Practical 6/ErlangIntergration/ErlangIntergration/ErlangIntergration.cpp
@@ -1,20 +1,122 @@
 // ErlangIntergration.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "erl_comm.h"
+#include "port_driver.h"
+
+// port
+typedef unsigned char byte;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+
+	ETERM* tuplep, * intp;
+	ETERM* fnp, * argp;
+
+	int res; // , fn, arg;
+	byte buf[100];
+	long allocated, freed;
+
+	erl_init(NULL, 0);
+
+	while (read_cmd(buf) > 0) 
+	{
+		/*fn = buf[0];
+		arg = buf[1];
+
+		if (fn == 1) 
+		{
+			res = foo(arg);
+		}
+		else if (fn == 2) 
+		{
+			res = bar(arg);
+		}
+
+		buf[0] = res;
+		write_cmd(buf, 1);*/
+		
+		tuplep = erl_decode(buf);
+		fnp = erl_element(1, tuplep);
+		argp = erl_element(2, tuplep);
+
+		if (strncmp(ERL_ATOM_PTR(fnp), "foo", 3) == 0) 
+		{
+			res = foo(ERL_INT_VALUE(argp));
+		}
+		else if (strncmp(ERL_ATOM_PTR(fnp), "bar", 3) == 0) 
+		{
+			res = bar(ERL_INT_VALUE(argp));
+		}
+
+		intp = erl_mk_int(res);
+		erl_encode(intp, buf);
+		write_cmd(buf, erl_term_len(intp));
+
+		erl_free_compound(tuplep);
+		erl_free_term(fnp);
+		erl_free_term(argp);
+		erl_free_term(intp);
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//
+int read_cmd(byte* buf)
+{
+	int len;
+
+	if (read_exact(buf, 2) != 2)
+		return(-1);
+
+	len = (buf[0] << 8) | buf[1];
+
+	return read_exact(buf, len);
+}
+
+int write_cmd(byte* buf, int len)
+{
+	byte li;
+
+	li = (len >> 8) & 0xff;
+	write_exact(&li, 1);
+
+	li = len & 0xff;
+	write_exact(&li, 1);
+
+	return write_exact(buf, len);
+}
+
+int read_exact(byte* buf, int len)
+{
+	int i, got = 0;
+	std::ifstream is ("", std::ifstream::binary);
+
+	do 
+	{
+		if ((i = is.read((char*)buf + got, len - got)) <= 0) // read(0, buf + got, len - got)) <= 0)
+			return(i);
+		got += i;
+	}
+	while (got < len);
+
+	return(len);
+}
+
+int write_exact(byte* buf, int len)
+{
+	int i, wrote = 0;
+	std::ofstream os("", std::ifstream::binary);
+
+	do 
+	{
+		if ((i = os.write((char*)buf + wrote, len - wrote)) <= 0) // write(1, buf + wrote, len - wrote)) <= 0)
+			return (i);
+		
+		wrote += i;
+	}
+	while (wrote < len);
+
+	return (len);
+}
